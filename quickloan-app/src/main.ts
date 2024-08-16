3fe404218f66778098fa78d4ce8c41998a444515
diego97jaramillo <diego97jaramillo@gmail.com>
2024-08-15 20:00:03 -0500

    taller final

--- quickloan-app/src/main.ts
+++ quickloan-app/src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpErrorFilter } from './common/http-exception.filter';
 
 // En main.ts
-
-
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpErrorFilter());
   await app.listen(3000);
 }
-
 bootstrap();
